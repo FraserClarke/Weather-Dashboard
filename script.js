1ef90440ebec9cff904be3b52a0c576b9aca95e5
Mona Lisa <clarkefraser1@gmail.com>
2020-12-21 15:42:11 +1100

    Adding classes to html, creating functions for js, ajax, city search, temp, humidity

--- script.js
+++ script.js
@@ -13,24 +13,87 @@ WHEN I open the weather dashboard
 THEN I am presented with the last searched city forecast
 */
 
+//local storage example: city: ["paris", "london", "sydney"]
+
+
+//on click function getting users input
+$(document).ready(function() {
+
+	//need onclick event
+	$("#citySearchBtn").on("click", grabInput)
+
+	//grab city name 
+	function grabInput(){
+	
+		var city = $("#cityName").val();
+		// if city is empty
+			// alter city cannot be empty
+		searchCityWeather(city);
+	}
+    //searching api, gathering data
+	function searchCityWeather(city){
+		console.log(city);                                           //using back tick,Template literals/strings
+		var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=+${city}+&appid=5112d0108f86dc264863990862ea0d1c`
+		console.log(queryURL);
+		$.ajax({
+			type: "GET",
+			url: queryURL,
+			dataType: "json",
+			success: function(response){
+				console.log(response)
+                //gathering temperature data converting it from kelvin to celcius
+                var temp = parseInt(response.main.temp)-273.15;
+				console.log(Math.round(temp * 100) / 100+ " °C");
+
+				var humid = $("<p>").addClass("weather-text").text(`Humidity: ${response.main.humidity} %`);
+				console.log(humid);
+
+				$(".display").append(humid);
+
+				var temp;
+
+
+				// query URL for uv index
+				// var uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=5112d0108f86dc264863990862ea0d1c&lat="+${lat}+"&lon="+${long},`
+
+
+				//forecast query URL
+				// var forecastURL = `http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=5112d0108f86dc264863990862ea0d1c&units=imperial`
+
+			}
+		})
+
+	}
 
 
 
 
 
-/* need onclick event
-need ajax function, get data for
--current and future conditions
-Block 1 top
--city name, the date, an icon representation of weather conditions, 
-the temperature, the humidity, the wind speed, and the UV index
-Block 2 Below
--5 day forecast + conditions
--uv index icon
-Block 3 Left
-=Search funtion Displaying city names
--onlcik event
--icons
+	//need ajax function, get data for
+	// current condition
+	//build skeleton html
+	//ajax call for the city
+	//look at the response
+	//log relevant values 
+	//append values into the DOM
 
-Need function to convert kelvin to celcius
-*/
+	// 5 day forecast with 5 cards 
+
+	// and future conditions
+
+	// Block 1 top
+	// -city name, the date, an icon representation of weather conditions, 
+	// the temperature, the humidity, the wind speed, and the UV index
+	// Block 2 Below
+	// -5 day forecast + conditions
+	// -uv index icon
+	// Block 3 Left
+	// =Search funtion Displaying city names
+	// -onlcik event
+	// -icons
+
+	// Need function to convert kelvin to celcius
+
+})
+
+
