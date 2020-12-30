2a45d92ca1a0f655de502d2c02ffe16136df0a22
Mona Lisa <clarkefraser1@gmail.com>
2020-12-30 11:53:31 +1100

    creating lists for previous searches in js file, refining js file

--- script.js
+++ script.js
@@ -18,6 +18,45 @@ THEN I am presented with the last searched city forecast
 
 //on click function getting users input
 $(document).ready(function() {
+	var searchedCities = JSON.parse(localStorage.getItem("cities")) || []; 	
+
+	//function that intiialises when the page loads, if no cities searched displaying none
+	init()
+
+	function init(){
+		if (searchedCities.length > 0){
+			searchCityWeather(searchedCities[searchedCities.length-1])
+		}
+		$("#previousSearches ul").empty();
+		renderCities(searchedCities)
+	}
+	//clearing functions so they dont repeat on html.... add others relevent
+	function clear(){
+		$(".displayTemp").empty();
+		$(".displayHumid").empty();
+		$(".displayIndex").empty();
+	}
+	//for loop, going through the array, getting last searched city
+	function renderCities(cities){
+		for (var i = cities.length - 1; i >= 0; i--) {
+			makeRow(cities[i]);
+		}
+
+
+		// when page loads we want to get ls items
+		// loop thorugh the array 
+		// each item in array needs to be a separate li
+		// each li need to be appended to ul
+		// ul needs to be created and appened to the div with id previousSearches
+	}  
+	//creating list for cities searched
+	function makeRow(city){
+		var li = $("<li>");
+		li.text(city);
+		$("#previousSearches ul").append(li);
+	}
+	
+
 
 	//need onclick event
 	$("#citySearchBtn").on("click", grabInput)
@@ -27,14 +66,16 @@ $(document).ready(function() {
 	
 		var city = $("#cityName").val();
 		// if city is empty     === empty set "" ....empty string
-			// alter city cannot be empty
+		// alter city cannot be empty
 		searchCityWeather(city);
 	}
     //searching api, gathering data
 	function searchCityWeather(city){
-		console.log(city);                                       //using back tick,Template literals/strings
+		clear()
+		saveCityToLS(city);
+		makeRow(city)                                      //using back tick,Template literals/strings
 		var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=+${city}+&appid=5112d0108f86dc264863990862ea0d1c`
-		console.log(queryURL);
+		// console.log(queryURL);
 		$.ajax({
 			type: "GET",
 			url: queryURL,
@@ -51,7 +92,7 @@ $(document).ready(function() {
 
 				$(".displayHumid").append(humid);
                 //$(".display").append(temp);
-                var temp = (Math.round(temp * 100) / 100+ " °C");
+                var temp = "Temperature: " + (Math.round(temp * 100) / 100+ " °C");
                 $(".displayTemp").append(temp);
             
                 var windSpeed = parseInt(response.wind.speed);
@@ -59,36 +100,39 @@ $(document).ready(function() {
                 //multiply the speed value by 1.609
                 var windSpeed = ((windSpeed * 1.609)).toFixed(2);
                 $(".displaySpeed").append(windSpeed);
-            
+				// query URL for uv index + ajax
+		             //function lat + long 
+                var lon = response.coord.lon;
+                var lat = response.coord.lat;
+
+                var queryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=5112d0108f86dc264863990862ea0d1c&lat=${lat}&lon=${lon}`
+		        // console.log(queryURL);
+		        $.ajax({
+					type: "GET",
+					url: queryURL,
+					dataType: "json",
+					success: function(response){
+						var uvIndex = response.value;
+						$(".displayIndex").append(uvIndex);
+                    
+                }
+		        //          5 day forecast query URL + ajax
+		        // var forecastURL = `http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=5112d0108f86dc264863990862ea0d1c&units=imperial`
+   			 	})
             }
-				
-        			
+							
 		})
       
-                   
-        
-          }
+    }
     
-    // query URL for uv index + ajax
-		             //function lat + long                        
-             var queryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=5112d0108f86dc264863990862ea0d1c&lat="+${lat}+"&lon="+${long},`
-             $.ajax({
-                 type: "GET",
-                 url: queryURL,
-                 dataType: "json",
-                 success: function(response){
-                    console.log(response);
     
-       
-                 }
-                 //5 day forecast query URL + ajax
-        // var forecastURL = `http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=5112d0108f86dc264863990862ea0d1c&units=imperial`
-    
-    })
-
-    
-
-
+	//function pushing cities into the array, local storage
+	function saveCityToLS(cityName){
+		if (searchedCities.indexOf(cityName) === -1) {
+			searchedCities.push(cityName);
+			localStorage.setItem("cities", JSON.stringify(searchedCities))
+		}
+	}                	         
 })
 
 	//need ajax function, get data for
